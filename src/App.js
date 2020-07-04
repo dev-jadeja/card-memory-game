2a837a4b559fd32e9f0f2b95f3cda387b37f4c11
Dev Jadeja <devjadeja559@gmail.com>
2020-07-04 13:49:31 +0530

    before css modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const app = () => {
-  return (
-    <div>
-      hello
-    </div>
-  );
-}
+	return <div>hello</div>;
+};
 
 export default app;
